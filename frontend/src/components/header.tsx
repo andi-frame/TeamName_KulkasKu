de7d29940de9decc4a8d3a7744acec3002fdd52c
Barru Adi <barru.adi@gmail.com>
2025-07-13 23:51:14 +0700

    feat: header

---
+++ frontend/src/components/header.tsx
@@ -0,0 +1,26 @@
+import { MenuBar } from "./menu-bar"
+import { Search } from "lucide-react"
+import { ArrowDownWideNarrow } from "lucide-react"
+
+export function Header() {
+    return (
+        <div className="flex flex-col p-6 pb-0 gap-6 shadow-md">
+            <div className="w-full inline-flex flex-col justify-start items-start gap-6">
+                <div className="relative">
+                    <Search size={24} strokeWidth={2}/>
+                </div>
+                <div className="self-stretch inline-flex justify-between items-center">
+                    <div className="justify-start text-2xl font-semibold leading-snug">
+                        KulkasKu
+                    </div>
+                    <div className="w-7 h-7 relative overflow-hidden">
+                        <ArrowDownWideNarrow size={28} strokeWidth={1} />
+                    </div>
+                </div>
+                <div className="overflow-x-auto whitespace-nowrap scroll-smooth">
+                    <MenuBar/>
+                </div>
+            </div>
+        </div>
+    )
+}
