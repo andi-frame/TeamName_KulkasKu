1b3061fa5a95f76959717abd2b80008e4d4096ee
andi-frame <andifarhan1094@gmail.com>
2025-07-21 14:48:17 +0800

    feat: add schema for recipe and recipe detail

---
+++ backend/schema/recipe.schema.go
@@ -0,0 +1,49 @@
+package schema
+
+type Author struct {
+	ID          string `json:"id"`
+	UUID        string `json:"uuid"`
+	Username    string `json:"username"`
+	Name        string `json:"name"`
+	Avatar      string `json:"avatar"`
+	IsFollowing bool   `json:"is_following"`
+	IsOfficial  bool   `json:"is_official"`
+}
+
+type Tag struct {
+	Name string `json:"name"`
+	Slug string `json:"slug"`
+}
+
+type Category struct {
+	Name string `json:"name"`
+	Slug string `json:"slug"`
+}
+
+type Recook struct {
+	RecookCount int           `json:"recook_count"`
+	RecookList  []interface{} `json:"recook_list"`
+}
+
+type Recipe struct {
+	ID             string   `json:"id"`
+	Title          string   `json:"title"`
+	Slug           string   `json:"slug"`
+	CoverURL       string   `json:"cover_url"`
+	Rating         int      `json:"rating"`
+	CookingTime    int      `json:"cooking_time"`
+	Calories       string   `json:"calories"`
+	ReleaseDate    int64    `json:"release_date"`
+	IsEditorial    bool     `json:"is_editorial"`
+	StepCount      int      `json:"step_count"`
+	PremiumContent bool     `json:"premium_content"`
+	VisitedCount   int      `json:"visited_count"`
+	IsBookmark     bool     `json:"is_bookmark"`
+	UpdatedAt      int64    `json:"updated_at"`
+	Subcategory    string   `json:"subcategory"`
+	Price          int      `json:"price"`
+	Author         Author   `json:"author"`
+	Recook         Recook   `json:"recook"`
+	Category       Category `json:"category"`
+	Tags           []Tag    `json:"tags"`
+}
---
+++ backend/schema/recipe_detail.schema.go
@@ -0,0 +1,74 @@
+package schema
+
+type CookingStep struct {
+	Title         string `json:"title"`
+	Text          string `json:"text"`
+	ImageURL      string `json:"image_url"`
+	Order         int    `json:"order"`
+	StartAt       int    `json:"start_at"`
+	OriginalImage string `json:"original_image"`
+}
+
+type Ingredient struct {
+	Description           string      `json:"description"`
+	Recommendation        string      `json:"recommendation"`
+	Brand                 string      `json:"brand"`
+	BuyURL                string      `json:"buy_url"`
+	MediaURL              string      `json:"media_url"`
+	RecomendationMediaURL string      `json:"recomendation_media_url"`
+	RelatedRecipe         interface{} `json:"related_recipe"`
+}
+
+type IngredientType struct {
+	Name        string       `json:"name"`
+	Ingredients []Ingredient `json:"ingredients"`
+}
+
+type RecipeDetail struct {
+	ID              string           `json:"id"`
+	Title           string           `json:"title"`
+	Slug            string           `json:"slug"`
+	CoverURL        string           `json:"cover_url"`
+	CoverWatermark  string           `json:"cover_url_watermark"`
+	Description     string           `json:"description"`
+	Rating          int              `json:"rating"`
+	VideoURL        string           `json:"video_url"`
+	Price           int              `json:"price"`
+	PricePremium    int              `json:"price_premium"`
+	Calories        string           `json:"calories"`
+	IsCVC           bool             `json:"is_cvc"`
+	CookingTime     int              `json:"cooking_time"`
+	ServingMin      int              `json:"serving_min"`
+	ServingMax      int              `json:"serving_max"`
+	ReleaseDate     int64            `json:"release_date"`
+	UpdatedDate     int64            `json:"updated_date"`
+	IsBookmark      bool             `json:"is_bookmark"`
+	IsEditorial     bool             `json:"is_editorial"`
+	RatingUser      int              `json:"rating_user"`
+	IsVideo         bool             `json:"is_video"`
+	PremiumContent  bool             `json:"premium_content"`
+	BrandSlug       string           `json:"brand_slug"`
+	IngredientCount int              `json:"ingredient_count"`
+	MetaTitle       string           `json:"meta_title"`
+	MetaDescription string           `json:"meta_description"`
+	OGCaption       string           `json:"og_caption"`
+	OGTitle         string           `json:"og_title"`
+	OGDescription   string           `json:"og_description"`
+	OGMedia         string           `json:"og_media"`
+	RaterCount      int              `json:"rater_count"`
+	RecookCount     int              `json:"recook_count"`
+	OriginalImage   string           `json:"original_image"`
+	ShareLink       string           `json:"share_link"`
+	Category        Category         `json:"category"`
+	CookingStep     []CookingStep    `json:"cooking_step"`
+	Tags            []Tag            `json:"tags"`
+	TagIngredients  []Tag            `json:"tag_ingredients"`
+	RecipeInfo      []interface{}    `json:"recipe_info"`
+	Author          Author           `json:"author"`
+	IngredientType  []IngredientType `json:"ingredient_type"`
+	Nutrition       []interface{}    `json:"nutrition"`
+	Recook          []interface{}    `json:"recook"`
+	CookingTool     []interface{}    `json:"cooking_tool"`
+	TipsAndTrick    []interface{}    `json:"tips_and_trick"`
+	PurchaseDetail  []interface{}    `json:"purchase_detail"`
+}
