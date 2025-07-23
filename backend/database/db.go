c211d2f798eacc232d46b8131f8355496f5bd91c
andi-frame <andifarhan1094@gmail.com>
2025-07-24 07:41:49 +0800

    feat: add prefer simple protocol

--- backend/database/db.go
+++ backend/database/db.go
@@ -14,22 +14,20 @@ var DB *gorm.DB
 func Init() {
 	dsn := os.Getenv("DATABASE_URL")
 	var err error
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	DB, err = gorm.Open(postgres.New(postgres.Config{
+		DSN:                  dsn,
+		PreferSimpleProtocol: true,
+	}), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
 	}
 
-	err = DB.AutoMigrate(
+	if err := DB.AutoMigrate(
 		&schema.User{},
 		&schema.Item{},
 		&schema.UserActivity{},
 		&schema.UserPreference{},
-		// &schema.Recipe{},
-		// &schema.RecipeStep{},
-		// &schema.Ingredient{},
-		// &schema.RecipeIngredient{},
-	)
-	if err != nil {
-		log.Fatal("Failed to migrate:", err)
+	); err != nil {
+		log.Println("AutoMigrate warning:", err)
 	}
 }
