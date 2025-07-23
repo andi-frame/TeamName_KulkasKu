414c554c298079c29e1b0b242bf73e3be82fb3e7
Barru Adi <barru.adi@gmail.com>
2025-07-23 22:56:23 +0700

    fix: auth frotnend

--- frontend/src/components/auth/LoginButton.tsx
+++ frontend/src/components/auth/LoginButton.tsx
@@ -10,7 +10,7 @@ const LoginButton = () => {
 
   return (
     <button
-      className="cursor-pointer py-1 rounded-xl ring-1 my-5 flex items-center justify-center gap-2 px-8 font-bold whitespace-nowrap"
+      className="bg-[#5EB1FF] text-white cursor-pointer py-2 rounded-md ring-1 my-5 flex items-center justify-center gap-2 px-8 font-bold whitespace-nowrap"
       onClick={handleLogin}>
       <Image src="/auth/logo-google.svg" alt="Google Logo" height={20} width={20} />
       Masuk dengan Google
