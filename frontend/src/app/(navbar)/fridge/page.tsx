c14d187cb2dd12779a45745268a07e198bf3b1e0
varel183 <vareltiara@gmail.com>
2025-07-24 21:47:14 +0700

    feat: add filter and notif

--- frontend/src/app/(navbar)/fridge/page.tsx
+++ frontend/src/app/(navbar)/fridge/page.tsx
@@ -2,14 +2,22 @@
 
 import ExpiredGroup from "@/components/fridge/expired-group";
 import FreshGroup from "@/components/fridge/fresh-group";
+import FilterSortBar from "@/components/fridge/filter-sort-bar";
+import ExpiryNotification from "@/components/expiry-notification";
 
 export default function Fridge() { 
 
 
   return (
     <div className="pb-28">
-      <ExpiredGroup/>
-      <FreshGroup />
+      <ExpiryNotification />
+      <div className="sticky top-0 z-10">
+        <FilterSortBar />
+      </div>
+      <div className="px-4">
+        <ExpiredGroup/>
+        <FreshGroup />
+      </div>
     </div>
   );
 }
