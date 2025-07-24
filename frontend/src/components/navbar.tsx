d3b0494b821198b55302776785abfaaca5358ab8
andi-frame <andifarhan1094@gmail.com>
2025-07-24 18:47:14 +0800

    feat: change history to cart

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,39 +1,32 @@
-"use client"
+"use client";
 
-import { 
-    PcCase, 
-    BrainCircuit, 
-    History, 
-    Settings, 
-    Plus 
-} from "lucide-react";
+import { PcCase, BrainCircuit, Settings, Plus, ShoppingCart } from "lucide-react";
 import Link from "next/link";
 
 export function Navbar() {
-    
-    return (
-        <div className="fixed bottom-0 left-0 w-full bg-white shadow-[0px_-4px_8px_0px_rgba(0,0,0,0.05)] flex justify-around items-center h-24">
-            <Link href={"/fridge"} className="flex flex-col items-center gap-2" >
-                <PcCase size={24} strokeWidth={1} />
-                <span className="text-xs font-normal leading-snug">kulkas</span>
-            </Link>
-            <Link href={"/recipe"} className="flex flex-col items-center gap-2" >
-                <BrainCircuit size={24} strokeWidth={1} />
-                <span className="text-xs font-normal leading-snug">resep.ai</span>
-            </Link>
-            <Link href={"/add-item"} className="relative flex flex-col items-center" >
-                <div className="absolute -top-16 w-16 h-16 bg-white rounded-full shadow-[0px_0px_8px_0px_rgba(0,0,0,0.10)] flex items-center justify-center">
-                    <Plus className="w-12 h-12" size={50} strokeWidth={1.25} color="#5DB1FF"/>
-                </div>
-            </Link>
-            <Link href={"/history"} className="flex flex-col items-center gap-2" >
-                <History size={24} strokeWidth={1} />
-                <span className="text-xs font-normal leading-snug">history</span>
-            </Link>
-            <Link href="/profile" className="flex flex-col items-center gap-2">
-                <Settings size={24} strokeWidth={1} />
-                <span className="text-xs font-normal leading-snug">profile</span>
-            </Link>
+  return (
+    <div className="fixed bottom-0 left-0 w-full bg-white shadow-[0px_-4px_8px_0px_rgba(0,0,0,0.05)] flex justify-around items-center h-24">
+      <Link href={"/fridge"} className="flex flex-col items-center gap-2">
+        <PcCase size={24} strokeWidth={1} />
+        <span className="text-xs font-normal leading-snug">kulkas</span>
+      </Link>
+      <Link href={"/recipe"} className="flex flex-col items-center gap-2">
+        <BrainCircuit size={24} strokeWidth={1} />
+        <span className="text-xs font-normal leading-snug">resep.ai</span>
+      </Link>
+      <Link href={"/add-item"} className="relative flex flex-col items-center">
+        <div className="absolute -top-16 w-16 h-16 bg-white rounded-full shadow-[0px_0px_8px_0px_rgba(0,0,0,0.10)] flex items-center justify-center">
+          <Plus className="w-12 h-12" size={50} strokeWidth={1.25} color="#5DB1FF" />
         </div>
-    );
+      </Link>
+      <Link href={"/shopping-cart"} className="flex flex-col items-center gap-2">
+        <ShoppingCart size={24} strokeWidth={1} />
+        <span className="text-xs font-normal leading-snug">cart</span>
+      </Link>
+      <Link href="/profile" className="flex flex-col items-center gap-2">
+        <Settings size={24} strokeWidth={1} />
+        <span className="text-xs font-normal leading-snug">profile</span>
+      </Link>
+    </div>
+  );
 }
