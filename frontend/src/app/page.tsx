4fe0490f4e9e2ecc865fa10eb8c82db790f75ea9
andi-frame <andifarhan1094@gmail.com>
2025-07-24 13:41:30 +0800

    feat: redirect auth

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,31 @@
+"use client";
+
 import { Header } from "@/components/header";
 import { Navbar } from "@/components/navbar";
+import api from "@/utils/axios";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 // import { FoodScanner } from "@/components/food-scanner";
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    const checkAuthUser = async () => {
+      try {
+        const res = await api.get("/auth/me");
+        if (res.status === 200) {
+          router.push("/fridge");
+        } else {
+          router.push("/auth");
+        }
+      } catch (error: unknown) {
+        console.error("Auth check failed:", error);
+        router.push("/auth");
+      }
+    };
+    checkAuthUser();
+  }, [router]);
+
   return (
     <div className="bg-white">
       <Header></Header>
@@ -19,7 +42,6 @@ export default function Home() {
         onImageResult={(result) => console.log("Image result:", result)}
         onReceiptResult={(result) => console.log("Receipt result:", result)}
       /> */}
-
     </div>
   );
 }
