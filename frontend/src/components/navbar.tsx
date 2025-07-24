f4a0c327af96e7ee4da60290b1c752d4587f7466
andi-frame <andifarhan1094@gmail.com>
2025-07-24 14:05:14 +0800

    feat: done add routing in navbar

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -7,34 +7,33 @@ import {
     Settings, 
     Plus 
 } from "lucide-react";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export function Navbar() {
-    const router = useRouter();
     
     return (
         <div className="fixed bottom-0 left-0 w-full bg-white shadow-[0px_-4px_8px_0px_rgba(0,0,0,0.05)] flex justify-around items-center h-24">
-            <div className="flex flex-col items-center gap-2" onClick={() => router.push("/")} >
+            <Link href={"/fridge"} className="flex flex-col items-center gap-2" >
                 <PcCase size={24} strokeWidth={1} />
                 <span className="text-xs font-normal leading-snug">kulkas</span>
-            </div>
-            <div className="flex flex-col items-center gap-2" onClick={() => router.push("/")} >
+            </Link>
+            <Link href={"/recipe"} className="flex flex-col items-center gap-2" >
                 <BrainCircuit size={24} strokeWidth={1} />
-                <span className="text-xs font-normal leading-snug">AI</span>
-            </div>
-            <div className="relative flex flex-col items-center" onClick={() => router.push("/add-item")} >
+                <span className="text-xs font-normal leading-snug">resep.ai</span>
+            </Link>
+            <Link href={"/add-item"} className="relative flex flex-col items-center" >
                 <div className="absolute -top-16 w-16 h-16 bg-white rounded-full shadow-[0px_0px_8px_0px_rgba(0,0,0,0.10)] flex items-center justify-center">
                     <Plus className="w-12 h-12" size={50} strokeWidth={1.25} color="#5DB1FF"/>
                 </div>
-            </div>
-            <div className="flex flex-col items-center gap-2" onClick={() => router.push("/")} >
+            </Link>
+            <Link href={"/history"} className="flex flex-col items-center gap-2" >
                 <History size={24} strokeWidth={1} />
                 <span className="text-xs font-normal leading-snug">history</span>
-            </div>
-            <div className="flex flex-col items-center gap-2" onClick={() => router.push("/profile")} >
+            </Link>
+            <Link href="/profile" className="flex flex-col items-center gap-2">
                 <Settings size={24} strokeWidth={1} />
                 <span className="text-xs font-normal leading-snug">profile</span>
-            </div>
+            </Link>
         </div>
     );
 }
