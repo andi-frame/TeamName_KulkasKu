e36b100ac4e0ea399914149c206c8a58c82f7dbc
Barru Adi <barru.adi@gmail.com>
2025-08-27 15:12:42 +0700

    fix: fridge expired column

--- frontend/src/app/(navbar)/fridge/page.tsx
+++ frontend/src/app/(navbar)/fridge/page.tsx
@@ -1,12 +1,13 @@
-// import { FoodCard } from "@/components/food-card";
+"use client"
 
 import ExpiredGroup from "@/components/fridge/expired-group";
 import FreshGroup from "@/components/fridge/fresh-group";
 import FilterSortBar from "@/components/fridge/filter-sort-bar";
 import ExpiryNotification from "@/components/expiry-notification";
+import { useExpiredStore } from "@/store/useExpiredStore";
 
 export default function Fridge() { 
-
+  const hasExpiredItems = useExpiredStore((state) => state.isExpired);
 
   return (
     <div className="pb-28">
@@ -17,11 +18,11 @@ export default function Fridge() {
 
       <div className="px-4 md:px-6 w-full">
         <div className="grid grid-cols-1 md:grid-cols-3 md:gap-6 w-full">
-          <section className="md:col-span-1 space-y-4">
-            <ExpiredGroup />
-          </section>
+            <section className="md:col-span-1 space-y-4">
+              <ExpiredGroup />
+            </section>
 
-          <section className="md:col-span-2">
+          <section className={hasExpiredItems ? "md:col-span-2" : "md:col-span-3"}>
             <FreshGroup />
           </section>
         </div>
